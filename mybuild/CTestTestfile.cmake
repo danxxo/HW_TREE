aa70797a06e892daf41470db4db18c6c9accad2c
danxxo <90875721+danxxo@users.noreply.github.com>
2022-06-06 13:44:33 +0300

    ignore

--- mybuild/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nikita/Desktop/TREEEE/cmake-example-repo
-# Build directory: /home/nikita/Desktop/TREEEE/cmake-example-repo/mybuild
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/nikita/Desktop/TREEEE/cmake-example-repo/mybuild/example_tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
